f1be48a8f3bc45452ed4c8a22020980955312b89
dneetuaws <dneetuaws@gmail.com>
2022-10-21 00:13:50 +0530

    Added  RG

--- resurgrp.tf
+++ resurgrp.tf
@@ -2,13 +2,3 @@ resource "azurerm_resource_group" "devopstf" {
   name     = "devopstf"
   location = "westus"
 }
-
-resource "azurerm_resource_group" "devopstf1" {
-  name     = "devopstf1"
-  location = "eastus"
-}
-
-resource "azurerm_resource_group" "devopstf2" {
-  name     = "devopstf2"
-  location = "westus"
-}

9b6ab680c0e4bca2be8ab48003e23591797cfcba
dneetuaws <dneetuaws@gmail.com>
2022-10-20 19:33:55 +0530

    Added 3 RG

--- Provider.tf
+++ Provider.tf
@@ -1,10 +1,10 @@
 # configure Microsoft Azure Provider
 
 provider "azurerm" {
-   features{
-   # subscription_id = ""
-   # client_id       = ""
-   # client_secret   = "" 
-       
-   }
+  features {
+    # subscription_id = ""
+    # client_id       = ""
+    # client_secret   = "" 
+
+  }
 }
--- resurgrp.tf
+++ resurgrp.tf
@@ -1,4 +1,14 @@
 resource "azurerm_resource_group" "devopstf" {
   name     = "devopstf"
-  location = "westus1"
+  location = "westus"
+}
+
+resource "azurerm_resource_group" "devopstf1" {
+  name     = "devopstf1"
+  location = "eastus"
+}
+
+resource "azurerm_resource_group" "devopstf2" {
+  name     = "devopstf2"
+  location = "westus"
 }

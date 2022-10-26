82b0f7be0278377d1686b107b3c396ef2e9632fc
dneetuaws <dneetuaws@gmail.com>
2022-10-26 14:37:20 +0530

    Added subnets

---
+++ subnets.tf
@@ -0,0 +1,23 @@
+resource "azurerm_subnet" "subnet-1" {
+  name                 = "azsubnet-1"
+  resource_group_name  = azurerm_resource_group.devopstf.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.26.1.0/24"]
+
+}
+
+resource "azurerm_subnet" "subnet-2" {
+  name                 = "azsubnet-2"
+  resource_group_name  = azurerm_resource_group.devopstf.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.26.2.0/24"]
+
+}
+
+resource "azurerm_subnet" "subnet-3" {
+  name                 = "azsubnet-3"
+  resource_group_name  = azurerm_resource_group.devopstf.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.26.3.0/24"]
+
+}
